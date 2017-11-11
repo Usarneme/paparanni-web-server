39bbc91270c4df32f234a10e285dca66513d45c5
usarneme <jamestcoop@gmail.com>
2017-11-10 23:14:39 -0800

    Style adjustments.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,8 +1,7 @@
-var tagInput = document.getElementById('tagInput');
+const tagInput = document.getElementById('tagInput');
 
 // Function on a Tag type button click adds that tag to the tags filter field input
 var addTag = function(event) {
-    console.log('button click with event: '+event);
     event.preventDefault();
     tagInput.value = tagInput.value + ' ' + event.innerText;
 }
